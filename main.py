36be01cf5003ceba8680ecb434469822c81a59cc
libre-land <steadstamina@gmail.com>
2021-01-02 04:33:47 -0800

    added start screen

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import kivy
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+
+
+Window.size = (414, 736)
+
+
+class StartScreen(Screen):
+    pass
 
 
 class MainScreen(Screen):
@@ -8,8 +15,13 @@ class MainScreen(Screen):
 
 
 class WabilApp(MDApp):
-    def built(self):
-        return MainScreen()
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(StartScreen(name='start_screen'))
+        sm.add_widget(MainScreen(name='main_screen'))
+
+        return sm
+
 
 if __name__ == '__main__':
     app = WabilApp()
